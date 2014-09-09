1eddd9a6b67a7fab75fd7c92d9bc19766a2b48b6
meatball1982 <meatball1982@163.com>
2014-09-09 10:20:42 +0800

    gen int noh name

--- work_ANN_016_input_02_load_en_ch_cluster_int_noh_atom_name.m
+++ work_ANN_016_input_02_load_en_ch_cluster_int_noh_atom_name.m
@@ -2,16 +2,17 @@ clear all
 clc
 
 %% outline
-% gen the noh atom - atom int  
-% draw from 44986 * 63 matrix.
-% this m file draw the index
+% gen the noh atom  int name and index
 
 
 %% load data
 load Mat_016_cluster_3_20Dia_int.mat
 
-ind_int=load('../sh_file/work_draw_int_noh_ind/ind_int.txt');
-ind_at=load('../sh_file/work_draw_int_noh_ind/ind_at.txt');
+ind_int = load('../sh_file/work_draw_int_noh_ind/ind_int.txt');
+ind_at  = load('../sh_file/work_draw_int_noh_ind/ind_at.txt');
+na_at   = {'C','C','O','H','H','H','N','C','C','O','H','H','C',...
+           'O','H','H','H','N','C','H','H','H','H'};
+
 
 % Ind_Dia=[49,53,58];
 Ind_Dia=[49,54,58];
@@ -24,6 +25,11 @@ dia_noh=[];
 ang_noh=[];
 bod_noh=[];
 
+dia_na=[];
+ang_na=[];
+bod_na=[];
+
+
 % choose bond length
 for i=2:23
     ind1=a(i,1);
@@ -32,9 +38,10 @@ for i=2:23
     Ind_noh=[Ind_noh,tm];
     if tm~=0
         bod_noh=[bod_noh;ind1,ind2];
+        bod_na=[bod_na;na_at(ind1),mat2str(ind1-1),na_at(ind2),mat2str(ind2-1)];
     end
 end
-% ind_noh
+bod_na
 
 % choose bond angle
 con_ang=0;
@@ -47,9 +54,13 @@ for i=3:23
     if tm~=0
         con_ang=con_ang+1;
         ang_noh=[ang_noh;ind1,ind2,ind3];
+        ang_na=[ang_na;na_at(ind1),mat2str(ind1-1),...
+                       na_at(ind2),mat2str(ind2-1),...
+                       na_at(ind3),mat2str(ind3-1)];
     end
 end
-% ind_noh
+ang_na
+
 
 % choose dia 
 dia_all=[]
@@ -62,19 +73,17 @@ for i=4:23
     dia_all=[dia_all;ind1-1,ind2-1,ind3-1,ind4-1,tm];
     if tm==1
         dia_noh=[dia_noh;ind1,ind2,ind3,ind4];
+        dia_na=[dia_na;na_at(ind1),mat2str(ind1-1),...
+                       na_at(ind2),mat2str(ind2-1),...
+                       na_at(ind3),mat2str(ind3-1),...
+                       na_at(ind4),mat2str(ind4-1)];
     end
         
     Ind_noh=[Ind_noh,tm];    
 end
+dia_na
 
-Ind_noh=logical(Ind_noh);
-dat320_in_noh=dat_in(:,Ind_noh);
-dat320_in_dia=dat_in(:,Ind_Dia);
+save Mat_int_noh_name.mat bod_na ang_na dia_na
 
-clear a b i ind1 ind2 ind3 ind4 n_clu_beg n_clu_end tm
-
-% save Mat_320_int_input_output.mat 
-% save Mat_320_int_input_output54.mat 
-
-%% logs 
-% mod : 16-Aug-2014 21:27:57
+%% logs
+% mod : 09-Sep-2014 10:19:35
