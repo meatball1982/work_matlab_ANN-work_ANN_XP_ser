31817c50ecffaeb420dfe64eef937b2e7793ffc1
meatball1982 <meatball1982@163.com>
2014-09-12 10:03:37 +0800

    12-Sep-2014 10:03:05 save feb bod

--- work_ANN_XP_027_int_04_find_error_big_check_all_int.m
+++ work_ANN_XP_027_int_04_find_error_big_check_all_int.m
@@ -1,5 +1,6 @@
 clear all
 clc
+clf
 
 %% outline
 % plot the all atom int
@@ -10,7 +11,12 @@ load Mat_016_cluster_3_20Dia_int.mat dat_En dat_in
 load Mat_find_err_big_near_ind.mat  ind_tr_320 en_er
 
 % plot index and name
-load Mat_int_noh_name.mat
+load Mat_int_name.mat
+
+n_bod=length(bod_na);
+n_ang=length(ang_na);
+n_bid=length(dia_na);
+
 
 dat_in_nr=dat_in(ind_tr_320,:);
 dat_en_nr=dat_En(ind_tr_320);
@@ -18,15 +24,22 @@ dat_en_nr=dat_En(ind_tr_320);
 ind_va_er=dat_En==en_er;
 dat_in_va_er=dat_in(ind_va_er,:);
 
-h=plot(dat_in_nr(:,1:22)','bo-')
+% 
+h=plot(dat_in_nr(:,1:n_bod)','bo-');
 hold on 
-plot(dat_in_va_er(1:22)','ro-')
+plot(dat_in_va_er(1:n_bod)','ro-');
+set(gca,'XTick',1:1:n_bod)
+set(gca,'XTickLabel',bod_na)
+rotateXLabels( gca, -90 )
+
+fi_na=['../imgs/en/27_int_dia_input_en/find_err_big_all_int/fig_feb_all_int_01_bod'];
+fun_work_li_035_myfig_out(h,fi_na,3);
 
 
 
 
-% plot()
 
 %% logs 
+% mod : 12-Sep-2014 10:02:00
 % mod : 11-Sep-2014 20:32:53
 % mod : 08-Sep-2014 15:21:01
